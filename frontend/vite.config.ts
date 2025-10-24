ac2ba63ecad2fd2326895ce818510addb6d3a77e
EveraldoGCH <everaldoguevara@gmail.com>
2025-10-24 15:08:26 -0300

    tanstack router

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import type { PluginOption } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }) as PluginOption,
+    react()
+  ],
   server: {
     port: 5173,
   },
-});
+})
